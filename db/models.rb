c28aa123bc7b4d72f181c21c3b9b01e38d6b6945
Jamie McClymont <jamie.mcclymont@gmail.com>
2014-06-22 10:08:16 +1200

    Add text and submitted data to testimonials

--- db/models.rb
+++ db/models.rb
@@ -1,13 +1,25 @@
 class Teacher < Sequel::Model
   one_to_many :testimonials
+
+  def num_testimonials_submitted
+    self.testimonials.count &:submitted
+  end
 end
 
 class Student < Sequel::Model
   one_to_many :testimonials
+
+  def num_testimonials_submitted
+    self.testimonials.count &:submitted
+  end
 end
 
 class Testimonial < Sequel::Model
   many_to_one :teacher
   many_to_one :student
+
+  def submit
+    self.update(:submitted => true)
+  end
 end
 
--- db/tables.rb
+++ db/tables.rb
@@ -1,17 +1,20 @@
 DB.create_table?(:teachers) do
   primary_key :id
-  String :name
-  String :email
+  String      :name,  :null => false
+  String      :email, :null => false
 end
 
 DB.create_table?(:students) do
   primary_key :id
-  String :name
-  String :email
+  String      :name,  :null => false
+  String      :email, :null => false
 end
 
 DB.create_table?(:testimonials) do
   primary_key :id
-  Integer :student_id
-  Integer :teacher_id
+  Integer     :student_id, :null => false
+  Integer     :teacher_id, :null => false
+
+  TrueClass   :submitted,  :null => false, :default => false
+  String      :text,       :text => true
 end
