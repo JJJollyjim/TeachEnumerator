e21eed4ce02c7cc2f75be8ebd6f46469dff24789
Jamie McClymont <jamie.mcclymont@gmail.com>
2014-07-04 14:33:13 +1200

    Update email sending script

--- send.rb
+++ send.rb
@@ -1,5 +1,5 @@
-puts 'Exiting for safety...'
-exit
+#puts 'Exiting for safety...'
+#exit
 
 require 'rubygems'
 require 'bundler/setup'
@@ -9,31 +9,39 @@ require 'aws/ses'
 require_relative 'db/db'
 
 ses = AWS::SES::Base.new(
-  :access_key_id     => ENV['AWS_SES_KEY_ID']
+  :access_key_id     => ENV['TE_AWS_SES_KEY_ID'],
   :secret_access_key => ENV['TE_AWS_SES_SECRET_ACCESS_KEY']
 )
 
 Teacher.each do |teacher|
-
   puts "Sending to #{teacher[:email]}:"
 
-  body = "Hello, #{teacher[:name]}," + "<br><br>" +
-         "Here are the students who have requested testimonials from you this year:" + "<br>" +
-         "<ul>"
+  url = "testimonials.kwiius.com/testimonials/#{teacher[:secret]}"
 
-  names = teacher.testimonials.map do |test|
-    test.student[:name]
-  end
+  body =  "<p>Hello #{teacher[:name]},</p>"
+  body << "<p>#{teacher.testimonials.count} students have requested testimonials from you this year.</p>"
+  body << "<p>Here is your testimonial link:</p>"
+  body << "<p><a href='#{url}'>#{url}</a></p>"
+  body << "<p>You may visit this link at any time to write your testimonials up until Friday the 8th of August (the end of Week 3)</p>"
 
-  names.each do |name|
-    body << "<li>" + name + "</li>"
-  end
 
-  body << "</ul>" + "<br><br>" +
-          "<small>This is an automated system. Please report faults to jamie@kwiius.com</small>"
+#         "Here are the students who have requested testimonials from you this year:" + "<br>" +
+#         "<ul>"
+#
+#  names = teacher.testimonials.map do |test|
+#    test.student[:name]
+#  end
+#
+#  names.each do |name|
+#    body << "<li>" + name + "</li>"
+#  end
+#
+#  body << "</ul>" + "<br><br>" +
+#          "<small>This is an automated system. Please report faults to jamie@kwiius.com</small>"
 
   ses.send_email(
-    :to        => teacher[:email],
+#    :to        => teacher[:email],
+    :to        => 'jamie.mcclymont@gmail.com',
     :from      => '"WGC Testimonial System" <janet.mccallister@wgc.school.nz>',
     :subject   => 'Your Student Testimonial Applications',
     :html_body => body
